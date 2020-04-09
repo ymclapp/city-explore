6769bf742ced59656a127a6542ed4f6e77fec809
ymclapp <ymclapp@yahoo.com>
2020-04-09 10:01:16 -0500

    base back-end set up

--- js/server.js
+++ js/server.js
@@ -16,4 +16,65 @@ app.get('/', (request, response) => {
 
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+
+app.use(cors()); // Middleware
+
+app.get('/', (request, response) => {
+  response.send('City Explorer Goes Here');
+});
+
+app.get('/bad', (request, response) => {
+  throw new Error('oops');
+});
+
+app.get('/paypal', (request, response) => {
+  response.send(process.env.PAYPAL_URL);
+});
+
+app.get('/weather', (request, response) => {
+  response.send('Weather.');
+});
+
+// Add /location route
+app.get('/location', locationHandler);
+
+// Route Handler
+function locationHandler(request, response) {
+  const geoData = require('./data/geo.json');
+  const city = request.query.city;
+  const location = new Location(city, geoData);
+  response.send(location);
+}
+
+// Has to happen after everything else
+app.use(notFoundHandler);
+// Has to happen after the error might have occurred
+app.use(errorHandler); // Error Middleware
+
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+
+// Helper Functions
+
+function errorHandler(error, request, response, next) {
+  console.log(error);
+  response.status(500).json({
+    error: true,
+    message: error.message,
+  });
+}
+
+function notFoundHandler(request, response) {
+  response.status(404).json({
+    notFound: true,
+  });
+}
+
+function Location(city, geoData) {
+  this.search_query = city; // "cedar rapids"
+  this.formatted_query = geoData[0].display_name; // "Cedar Rapids, Iowa"
+  this.latitude = parseFloat(geoData[0].lat);
+  this.longitude = parseFloat(geoData[0].lon);
+}
 © 2020 GitHub, Inc.
+
