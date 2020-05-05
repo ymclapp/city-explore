854e451e57bbe54a4a41a50c2fc75513525a193c
ymclapp <ymclapp@yahoo.com>
2020-05-05 17:54:52 -0500

    add trails apli

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.get('/location', locationHandler);
 
 app.get('/weather', weatherHandler);
 
+app.get('/trails', trailsHandler);
+
 // Route Handler
 function locationHandler(request, response) {
   const city = request.query.city;
@@ -36,6 +38,7 @@ function locationHandler(request, response) {
   }).then((locIQresponse)=>{
     let geoData = locIQresponse.body;
     const location = new Location(city, geoData);
+    console.log(location);
     response.send(location);
   }).catch((error)=>{
     errorHandler(error,request,response);
@@ -60,6 +63,27 @@ function weatherHandler(request,response) {
     errorHandler(error,request,response);
   });
 }
+
+function trailsHandler(request,response) {
+  console.log(request.query);
+  let lat = parseFloat(request.query.latitude);
+  let lon = parseFloat(request.query.longitude);
+  const url = 'https://www.hikingproject.com/data/get-trails';
+  superagent.get(url).query({
+    key: process.env.TRAILS_API_KEY,
+    lon: lon,
+    lat: lat
+  }).then((hikersResponse)=>{
+    let trailsData = hikersResponse.body;
+    let trailsArray = trailsData.trails.map(trail=>{
+      return new Trails(trail);
+    });
+    response.send(trailsArray);
+  }).catch((error)=>{
+    errorHandler(error,request,response);
+  });
+}
+
 
 // Has to happen after everything else
 app.use(notFoundHandler);
@@ -93,6 +117,19 @@ function Weather(weatherData) {
   this.forecast = weatherData.weather.description;
   this.time = weatherData.ob_time;
 
+}
+
+function Trails(trailsData) {
+  this.name = trailsData.name;
+  this.location = trailsData.location;
+  this.length = trailsData.length;
+  this.stars = trailsData.stars;
+  this.star_votes = trailsData.starVotes;
+  this.summary = trailsData.summary;
+  this.trail_url = trailsData.trailURL;
+  this.condition_details = trailsData.conditionStatus;
+  this.condition_date = new Date (trailsData.conditionDate).toDateString;
+  this.condition_time = trailsData.conditionTime;
 }
 
 // Make sure the server is listening for requests
